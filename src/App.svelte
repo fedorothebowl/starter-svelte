739a5cc3e674943a71d0db9f83065d3e713c2071
federico petrucci <federico@quivi.it>
2024-03-23 11:21:29 +0100

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Test from './components/test.svelte'
+</script>
+
+<main>
+  <div class="grid place-items-center bg-red-200 h-screen w-full">
+    <Test />
+  </div>
+</main>
+
+<style>
+
+</style>
