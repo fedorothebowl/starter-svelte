90edcbac4e4358c113efea87f502147681cd38da
federico petrucci <federico@quivi.it>
2024-03-23 11:46:00 +0100

    mobile detector

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,17 @@
 <script>
   import Header from "./components/Header.svelte";
   import Footer from "./components/Footer.svelte";
+  import { isMobile } from "./resources/store/isMobile.js";
+
+  console.log($isMobile)
+
 </script>
 
 <main>
   <Header />
-  <div class="grid place-items-center bg-red-200 h-[80dvh] w-full">
-  </div>
+    <div class="grid place-items-center bg-red-200 h-[80dvh] w-full">
+      <div>Is your device a mobile phone? <span>{$isMobile}</span></div>
+    </div>
   <Footer />
 </main>
 
